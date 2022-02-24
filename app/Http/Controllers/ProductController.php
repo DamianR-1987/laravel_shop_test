0e93c7a52d8a9e13fcbcd38e93b7a69ce5dac69b
Damian R <d.rosinski@tryfon.eu>
2022-02-24 11:29:31 +0100

    CRUD for products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\Product;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index()
+    {
+        return view('products.index', [
+            'products' => Product::paginate(10)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return View
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $product = new Product($request->all());
+        $product->save();
+        return redirect(route('products.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Product  $product
+     * @return View
+     */
+    public function show(Product $product): View
+    {
+        return view('products.show', [
+            'product' => $product
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  Product  $product
+     * @return View
+     */
+    public function edit(Product $product): View
+    {
+        return view('products.edit', [
+            'product' => $product
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  Product  $product
+     * @return RedirectResponse
+     */
+    public function update(Request $request, Product $product): RedirectResponse
+    {
+        $product->fill($request->all());
+        $product->save();
+        return redirect(route('products.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Product  $product
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Product $product): \Illuminate\Http\JsonResponse
+    {
+        try{
+            $product->delete();
+            return response()->json([
+                'status' => 'success'
+            ]);
+        } catch (Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Wystąpił błąd!'
+            ])->setStatusCode(500);
+        }
+    }
+}
